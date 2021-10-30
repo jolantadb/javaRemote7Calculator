06c43249dc9eda36373ecfa8a0b9ada35838e762
Jolanta Baranovska <jolantadbaranovska@gmail.com>
2021-10-30 11:50:03 +0300

    Calculator part

--- src/carpet/Carpet.java
+++ src/carpet/Carpet.java
@@ -1,4 +1,21 @@
 package carpet;
 
 public class Carpet {
+
+    double cost;
+
+    public Carpet (double cost){
+        this.cost = cost;
+//        cost = 8.0;
+
+        if (cost < 0 ){
+            cost = 0;
+//            System.out.println("Cost: " + cost);
+        }
+    }
+
+    public  double getCost(){
+//        System.out.println(cost);
+        return cost;
+    }
 }
--- src/carpet/Floor.java
+++ src/carpet/Floor.java
@@ -1,4 +1,27 @@
 package carpet;
 
 public class Floor {
+
+   private double width;
+    private double length;
+
+
+public Floor(double width, double length){
+    this.length = length;
+    this.width = width;
+
+    if (width < 0){
+        length = 0;
+    }
+    if (length < 0){
+        width = 0;
+    }
+
+}
+
+public  double getArea (){
+    return (width * length);
+    }
+
+
 }
